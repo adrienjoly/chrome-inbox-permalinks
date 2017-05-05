84948fd30c2e111d61dc97d67e33fff4f149e70f
Adrien Joly <adrien@whyd.com>
2017-05-05 12:16:37 +0200

    initial commit: first working version (v0.1)

---
+++ README.md
@@ -0,0 +1,9 @@
+# Permalinks for Google Inbox (Chrome extension)
+
+A minimal extension that provides direct URLs to your emails, from Google Inbox.
+
+Install from Chrome Web Store: [TODO](TODO)
+
+### Demo
+
+TODO
---
+++ content.js
@@ -0,0 +1,20 @@
+function attachLinks(messageView) {
+	var url = 'https://mail.google.com/mail/#all/' + messageView.getMessageID();
+	// TODO: or https://mail.google.com/mail/h/ouilk5cfmn2l/?th=MESSAGE_ID&v=c ?
+	var threadView = messageView.getThreadView();
+	var div = document.createElement('div');
+	var link = document.createElement('a');
+	link.href = url;
+	link.target = '_blank';
+	link.innerHTML = 'Permalink to this email';
+	div.appendChild(link);
+	var res = threadView.addSidebarContentPanel({
+		el: div,
+		title: 'Permalink: ' + url,
+		iconUrl: chrome.extension.getURL('link.png'),
+	});
+}
+
+InboxSDK.load('2', 'sdk_inbox-permalink_77495e9cd5').then(function(sdk){
+	sdk.Conversations.registerMessageViewHandler(attachLinks);
+});
