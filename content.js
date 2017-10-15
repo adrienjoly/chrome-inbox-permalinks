e5fe189ba3e6c2fb937ff07c4df4630f7d62d796
Adrien Joly <adrien.joly@gmail.com>
2017-10-15 16:25:23 +0200

    v0.4: fixed permalink URLs (one per thread) according to inboxsdk’s update. + adjusted positioning of icon

--- content.js
+++ content.js
@@ -1,21 +1,34 @@
+/*
 function attachLinks(messageView) {
 	var url = 'https://mail.google.com/mail/#all/' + messageView.getMessageID();
 	// TODO: or https://mail.google.com/mail/h/ouilk5cfmn2l/?th=MESSAGE_ID&v=c ?
 	var threadView = messageView.getThreadView();
-	var div = document.createElement('div');
-	var link = document.createElement('a');
-	link.href = url;
-	link.target = '_blank';
-	link.innerHTML = 'Permalink to this email';
-	div.style = 'text-align: center;';
-	div.appendChild(link);
-	var res = threadView.addSidebarContentPanel({
-		el: div,
-		title: 'Permalink: ' + threadView.getSubject(),
-		iconUrl: chrome.extension.getURL('link.png'),
+	attachThreadLink(threadView);
+}
+*/
+function attachThreadLink(threadView) {
+	threadView.getThreadIDAsync().then(function(threadId) {
+		console.log('[google-inbox-permalinks] getThreadIDAsync => ', arguments);
+		if (!threadId) return;
+		var link = document.createElement('a');
+		link.href = 'https://mail.google.com/mail/#all/' + threadId;
+		link.target = '_blank';
+		link.appendChild(document.createTextNode('Permalink'));
+		// note: threadView.getSubject() contains junk in the French version of Google Inbox
+		var div = document.createElement('div');
+		//div.style = 'text-align: center;';
+		div.appendChild(link);
+		threadView.addSidebarContentPanel({
+			el: div,
+			title: 'Permalinks',
+			iconUrl: chrome.extension.getURL('link.png'),
+		});
+	}).catch(function() {
+		console.log('[google-inbox-permalinks] getThreadIDAsync => error:', arguments);
 	});
 }
 
 InboxSDK.load('2', 'sdk_inbox-permalink_77495e9cd5').then(function(sdk){
-	sdk.Conversations.registerMessageViewHandler(attachLinks);
+	//sdk.Conversations.registerMessageViewHandler(attachLinks);
+	sdk.Conversations.registerThreadViewHandler(attachThreadLink);
 });
